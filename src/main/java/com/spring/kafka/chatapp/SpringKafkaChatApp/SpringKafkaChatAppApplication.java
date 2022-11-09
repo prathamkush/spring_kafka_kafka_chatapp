4e160f3fa551530b2943daadcd2e76a669d64a71
prathamkush <Pratham.Kushwah@blueyonder.com>
2022-11-09 10:41:48 +0530

    running chat first

--- src/main/java/com/spring/kafka/chatapp/SpringKafkaChatApp/SpringKafkaChatAppApplication.java
+++ src/main/java/com/spring/kafka/chatapp/SpringKafkaChatApp/SpringKafkaChatAppApplication.java
@@ -2,7 +2,10 @@ package com.spring.kafka.chatapp.SpringKafkaChatApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+
+@EnableKafka
 @SpringBootApplication
 public class SpringKafkaChatAppApplication {
 
