f26cb0f094bdfe191ed0192c6dd063dedd8ad925
prathamkush <Pratham.Kushwah@blueyonder.com>
2022-11-09 09:24:00 +0530

    init with dependencies

---
+++ src/main/java/com/spring/kafka/chatapp/SpringKafkaChatApp/SpringKafkaChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.chatapp.SpringKafkaChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaChatAppApplication.class, args);
+	}
+
+}
